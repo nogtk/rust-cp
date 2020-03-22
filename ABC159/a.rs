506433add315f92e30abbe6d2d9bf1f089c3a1eb
nogtk <taka.naoga@moneyforward.co.jp>
2020-03-22 23:36:07 +0900

    hoge

---
+++ ABC159/a.rs
@@ -0,0 +1,67 @@
+fn read<T: std::str::FromStr>() -> T {
+  let mut s = String::new();
+  std::io::stdin().read_line(&mut s).ok();
+  s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+  read::<String>().split_whitespace()
+    .map(|e| e.parse().ok().unwrap()).collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+  (0..n).map(|_| read_vec()).collect()
+}
+
+pub trait LexicalPermutation {
+  /// Return `true` if the slice was permuted, `false` if it is already
+  /// at the last ordered permutation.
+  fn next_permutation(&mut self) -> bool;
+}
+impl<T> LexicalPermutation for [T] where T: Ord {
+  /// Original author in Rust: Thomas Backman <serenity@exscape.org>
+  fn next_permutation(&mut self) -> bool {
+    // These cases only have 1 permutation each, so we can't do anything.
+    if self.len() < 2 { return false; }
+
+    // Step 1: Identify the longest, rightmost weakly decreasing part of the vector
+    let mut i = self.len() - 1;
+    while i > 0 && self[i-1] >= self[i] {
+      i -= 1;
+    }
+
+    // If that is the entire vector, this is the last-ordered permutation.
+    if i == 0 {
+      return false;
+    }
+
+    // Step 2: Find the rightmost element larger than the pivot (i-1)
+    let mut j = self.len() - 1;
+    while j >= i && self[j] <= self[i-1]  {
+      j -= 1;
+    }
+
+    // Step 3: Swap that element with the pivot
+    self.swap(j, i-1);
+
+    // Step 4: Reverse the (previously) weakly decreasing part
+    self[i..].reverse();
+
+    true
+  }
+}
+fn main() {
+  let (n, m) = {
+    let tmp: Vec<isize> = read_vec();
+    (tmp[0], tmp[1])
+  };
+  let ans = combination(n, 2) + combination(m, 2);
+  println!("{}", ans);
+}
+
+fn combination(n: isize, r: isize) -> isize {
+  if n == 0 || n == 1 { 0 }
+  else if r == 0 || r == n { 1 }
+  else if r == 1 { n }
+  else { combination(n-1, r-1) + combination(n-1, r) }
+}
---
+++ ABC159/b.rs
@@ -0,0 +1,74 @@
+fn read<T: std::str::FromStr>() -> T {
+  let mut s = String::new();
+  std::io::stdin().read_line(&mut s).ok();
+  s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+  read::<String>().split_whitespace()
+    .map(|e| e.parse().ok().unwrap()).collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+  (0..n).map(|_| read_vec()).collect()
+}
+
+pub trait LexicalPermutation {
+  /// Return `true` if the slice was permuted, `false` if it is already
+  /// at the last ordered permutation.
+  fn next_permutation(&mut self) -> bool;
+}
+impl<T> LexicalPermutation for [T] where T: Ord {
+  /// Original author in Rust: Thomas Backman <serenity@exscape.org>
+  fn next_permutation(&mut self) -> bool {
+    // These cases only have 1 permutation each, so we can't do anything.
+    if self.len() < 2 { return false; }
+
+    // Step 1: Identify the longest, rightmost weakly decreasing part of the vector
+    let mut i = self.len() - 1;
+    while i > 0 && self[i-1] >= self[i] {
+      i -= 1;
+    }
+
+    // If that is the entire vector, this is the last-ordered permutation.
+    if i == 0 {
+      return false;
+    }
+
+    // Step 2: Find the rightmost element larger than the pivot (i-1)
+    let mut j = self.len() - 1;
+    while j >= i && self[j] <= self[i-1]  {
+      j -= 1;
+    }
+
+    // Step 3: Swap that element with the pivot
+    self.swap(j, i-1);
+
+    // Step 4: Reverse the (previously) weakly decreasing part
+    self[i..].reverse();
+
+    true
+  }
+}
+fn main() {
+  let s: String = read();
+  if check(&s) {
+    if check(&s[..s.len()/2].to_string()) && check(&s[(s.len()/2+1)..].to_string()) {
+      println!("Yes");
+    } else {
+      println!("No");
+    }
+  } else {
+    println!("No");
+  }
+
+}
+
+fn check(s: &String) -> bool {
+  let s: Vec<char> = s.chars().collect();
+  let mut ok = true;
+  for i in 0..(s.len()/2) {
+    if(s[i] != s[(s.len()-1)-i]) { ok = false; }
+  }
+  ok
+}
